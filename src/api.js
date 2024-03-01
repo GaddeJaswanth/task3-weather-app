9afeec84926830c4927f9c4b9d9a11fcb956069a
GaddeJaswanth <jaswanthgadde626@gmail.com>
2024-03-01 13:27:18 +0530

    Weather API

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': `//Rapid api key`,
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = `//Your OpenWeather api key`;
